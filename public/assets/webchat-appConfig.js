d050d4db5b9fce43e91fd6f1da03edbc9e586dd9
Gabriel Pérez <55561708+gperez-as@users.noreply.github.com>
2020-05-01 14:16:48 -0600

    twilio flex credentials added

---
+++ public/assets/webchat-appConfig.js
@@ -0,0 +1,7 @@
+var appConfig = {
+    //  change the your AccountSid
+    accountSid: "ACbca1d63020e4c2121c08289b8b4635a3",
+    // change to your Flex Flow SID
+    flexFlowSid: "FO1b48e7641e2633e0732e8fbc56270650",
+ 
+}
